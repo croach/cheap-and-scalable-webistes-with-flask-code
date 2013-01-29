a6c3b5e925d9b6ac2a8df2599fbd75f180f7cecf
Christopher Roach <vthakr@gmail.com>
2013-01-28 20:12:55 -0800

    Added the base and post templates

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/blog')
+def post():
+    return render_template('post.html')
 
 
 if __name__ == '__main__':

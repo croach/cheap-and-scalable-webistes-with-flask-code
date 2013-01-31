2bce1a13c456814defd5a831c1dd5227c1e410e8
Christopher Roach <vthakr@gmail.com>
2013-01-31 14:10:43 -0800

    Adding the url property to the Post class

--- generator.py
+++ generator.py
@@ -1,25 +1,30 @@
 import os
+import urlparse
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from werkzeug import cached_property
 import markdown
 import yaml
 
-
 class Post(object):
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, path, root=''):
+        self.urlpath = os.path.splitext(path.strip('/'))[0]
+        self.filepath = os.path.join(root, path.strip('/'))
         self._initialize_metadata()
 
     @cached_property
     def html(self):
-        with open(self.path, 'r') as fin:
+        with open(self.filepath, 'r') as fin:
             content = fin.read().split('\n\n', 1)[1].strip()
         return markdown.markdown(content)
 
+    @cached_property
+    def url(self):
+        return url_for('post', path=self.urlpath)
+
     def _initialize_metadata(self):
         content = ''
-        with open(self.path, 'r') as fin:
+        with open(self.filepath, 'r') as fin:
             for line in fin:
                 if not line.strip():
                     break
